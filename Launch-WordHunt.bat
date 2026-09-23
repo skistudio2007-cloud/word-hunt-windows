69631dde32e770d60d9f6befbec8b9486574d5d1
skistudio2007-cloud <skistudio2007@gmail.com>
2026-09-23 21:51:55 +0530

    fix: github actions pnpm setup and windows batch launcher

--- Launch-WordHunt.bat
+++ Launch-WordHunt.bat
@@ -1,13 +1,22 @@
 @echo off
 title Word Hunt - Windows Edition
-cd /d %~dp0
-if exist dist-electron\Word Hunt 1.0.0.exe (
-    start " dist-electron\Word Hunt 1.0.0.exe
- exit
+cd /d "%~dp0"
+
+if exist "dist-electron\win-unpacked\Word Hunt.exe" (
+    start "" "dist-electron\win-unpacked\Word Hunt.exe"
+    exit /b 0
 )
-if exist dist-electron\win-unpacked\Word Hunt.exe (
- start  dist-electron\win-unpacked\Word Hunt.exe
- exit
+
+if exist "dist-electron\Word Hunt 1.0.1.exe" (
+    start "" "dist-electron\Word Hunt 1.0.1.exe"
+    exit /b 0
 )
-echo Word Hunt executable not found!
+
+if exist "dist-electron\Word Hunt 1.0.0.exe" (
+    start "" "dist-electron\Word Hunt 1.0.0.exe"
+    exit /b 0
+)
+
+echo [ERROR] Word Hunt executable not found!
+echo Please check dist-electron folder.
 pause
